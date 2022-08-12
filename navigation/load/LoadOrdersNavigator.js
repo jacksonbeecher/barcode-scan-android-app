fa4adcdb8a0b88d3b97e3fb34220c0398cc8a701
Jackson Beecher <jbeecher@ashcom.com.au>
2022-08-12 11:44:27 +1000

    Implement UI elements for Load Orders Screen.

--- navigation/load/LoadOrdersNavigator.js
+++ navigation/load/LoadOrdersNavigator.js
@@ -1,7 +1,41 @@
-import { StyleSheet, View, Image, Text } from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 
 export default function LoadOrdersNavigator() {
     return (
-        <Text>Load Order: </Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>This function will load and prepare orders ready for packing.</Text>
+            <View style={styles.buttonContainer}>
+                <Button 
+                    style={styles.button}
+                    title="Load"
+                    onPress={() => {
+                        alert("Load Orders")
+                    }}
+                />
+                <Button
+                    style={styles.button}
+                    title="Cancel"
+                    onPress={() => {
+                        alert("backAction")
+                    }}
+                />
+            </View>
+        </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+
+    },
+    buttonContainer: {
+
+    },
+    button: {
+
+    },
+    text:{
+        fontSize:16,
+
+    },
+});
