d100bde9bbc909666dd1b6012ea40c3670a5b3ee
Jackson Beecher <jbeecher@ashcom.com.au>
2022-09-06 16:42:12 +1000

    Update bug in user select hooks.

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,20 +1,59 @@
+import { useEffect, useRef, useState } from "react";
 import { View, Modal, Text, Button } from "react-native";
+import DropDownPicker from "react-native-dropdown-picker";
+import { getUnitsFromApi } from '../component/api';
 
 const SettingsScreen = ({ navigation: { goBack } }) => {
+    const [open, setOpen] = useState(false); //Dropdownpicker open status.
+    const [unit, setUnit] = useState([]);
+    const [unitDS, setUnitDS] = useState([]);
+    const [isLoading, setLoading] = useState(false); //Use to hide/show loading animation.
+
+    //Load units from Api call.
+    async function fetchUnitData() {
+        console.log('fetch user data')
+        let data = await getUsersFromApi(); //data is in json format.
+        setUnitDS(data);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+
+        fetchUnitData();
+
+    }, []);
+
 
     return (
         <Modal animationType='slide'>
             <View>
                 <Text>
-                    Settings
+                    Settings:
                 </Text>
-                
+                <Text>Unit: {unit}</Text>
+                <DropDownPicker
+                    // open={open}
+                    // value={unit}
+                    // items={unitDS}
+                    // setOpen={setOpen}
+                    //setValue={setUnit}
+                    //setItems={setUnitDS}
+                    listMode="SCROLLVIEW"
+                    //itemKey="UnitId"
+                    closeAfterSelecting={true}
+                />
                 <Button
-                        title="Cancel"
-                        onPress={() => {
-                            goBack()
-                        }}
-                    />
+                    title="Cancel"
+                    onPress={() => {
+                        goBack()
+                    }}
+                />
+                <Button
+                    title="Save"
+                    onPress={() => {
+                        goBack()
+                    }}
+                />
             </View>
         </Modal>
     );
